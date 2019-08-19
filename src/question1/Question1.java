c777467de1cb45ac24e2f9214cc4ecf667a3b556
lenovo <anushka.maheshwari_cs18@gla.ac.in>
2019-08-19 02:02:16 -0700

    add output statement

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,4 +1,14 @@
 package question1;
 
+import java.util.Scanner;
 public class Question1 {
+    public static void main(String arg[]) {
+        String s;
+        System.out.print("write a string");
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+        sc.close();
+        System.out.print("Welcome, " + s + "!");
+    }
 }
+
