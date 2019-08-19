f1894290f7a2a318325b9adebf2acba143465836
lenovo <anushka.maheshwari_cs18@gla.ac.in>
2019-08-19 02:15:50 -0700

    add new file Question2.java

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,4 +1,16 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Question2 {
+    public static void main(String arg[]) {
+        String s1, s2;
+        Scanner sc = new Scanner(System.in);
+        s1 = sc.nextLine().trim();
+        s2 = sc.nextLine().trim();
+        sc.close();
+        System.out.print("" + s1 + " University " + s2);
+
+    }
 }
+
